2d61f27b24bd14ba34e420820ee40591fba80fdb
Andrei <andrei.massaini@hotmail.com>
2023-12-24 05:56:50 -0300

    first commit

---
+++ src/test/java/com/FaceCNN/faceRec/FaceRecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FaceCNN.faceRec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaceRecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
