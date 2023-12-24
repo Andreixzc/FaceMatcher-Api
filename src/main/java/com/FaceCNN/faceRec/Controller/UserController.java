021f01b401ff8ab310338e29b2b5d10b308659dd
Andrei <andrei.massaini@hotmail.com>
2023-12-24 06:27:02 -0300

    fixing user creation bugs

--- src/main/java/com/FaceCNN/faceRec/Controller/UserController.java
+++ src/main/java/com/FaceCNN/faceRec/Controller/UserController.java
@@ -2,6 +2,7 @@ package com.FaceCNN.faceRec.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +18,8 @@ public class UserController {
     @Autowired
     private UserService userService;
     
-    public ResponseEntity<?> createUser(@Valid @RequestBody User user){
+    @PostMapping()
+    public ResponseEntity<?> createUser(@RequestBody @Valid User user){
         return ResponseEntity.ok(userService.create(user));
     }
     
