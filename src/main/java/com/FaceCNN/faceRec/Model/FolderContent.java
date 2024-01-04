b6072fcae9315a15a2fa06434b9f15c18631c8d7
Andrei <andrei.massaini@hotmail.com>
2024-01-04 01:52:47 -0300

    Adding folerContent entity

---
+++ src/main/java/com/FaceCNN/faceRec/Model/FolderContent.java
@@ -0,0 +1,35 @@
+package com.FaceCNN.faceRec.Model;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class FolderContent {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(unique = true)
+    private String originalFileName;
+
+    @Column(unique = true)
+    private String pklFilename;
+
+    @OneToOne
+    @JoinColumn(name = "folder_id")
+    private Folder folder;  
+}
